9075052554d3c22c9043278a85f928bbe8c20213
Rodrigo Faria <rodrigo.f.ss@uol.com.br>
2017-09-29 20:29:45 -0300

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+//test.js
+const f2c = require('./index.js')
+ let tempC = f2c(20)
+ console.log(tempC)
+
+if(tempC == -6.67){
+	console.log('Teste OK!')
+	process.exit(0)
+}
+
+console.log('Teste failed!')
+process.exit(1)
